b84b624b8db448ea1acf3adcd1c85eac6afeab2b
Piotr Przeliorz <piotr.przeliorz@zaven.co>
2017-12-15 09:21:48 +0100

    Ajdust script to work with raw data

--- pressure.m
+++ pressure.m
@@ -3,12 +3,28 @@
 clear all;
 close all;
 clc;
+
+%% Extract data from source
+
+data = xlsread('st2500_S2_2x156f03v200_001.xls');
+
+resistance1 =  data(:,6);
+resistance2 = data(:,7);
+timestamps = data(:,10);
+
+
+%% Remove nan values from numeric vector and timestamps
+
+resistance1(isnan(resistance1)) = [];
+resistance2(isnan(resistance2)) = [];
+timestamps(isnan(timestamps)) = [];
+
+%% Sum of resistance
+
+sumOfReistance = resistance1 + resistance2
  
-data = xlsread('test.xls');
- 
-x = data(:,1);
-y = data(:,2);
- 
+x = timestamps
+y = sumOfReistance
 
 Y=y;
 Y(y<43)=0;
@@ -19,7 +35,7 @@ U=I;
 for i=2:length(X)
   
     if X(i)-U(end)>80
-          disp(X(i))
+%           disp(X(i))
     disp(U(end))
 %         disp(X(1))
         U=[U X(i)];
@@ -47,9 +63,11 @@ for i=1:length(U)
 %     disp(d)
     w=[w (u-d)/2];
 end
+
 plot(x,y);
+xlabel('Time'), ylabel('Force')
+title('Chart')
 
 disp(w')
  
-xlabel('Time'), ylabel('Force')
-title('Chart')
+
